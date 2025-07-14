a4d92b1bc77b6b675c7c9d8a600a53985c10a5f8
ted.diplock <ted.diplock@QR-GM0SH42F>
2025-07-14 11:53:00 -0400

    Add project files.

---
+++ Loader.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import pandas as pd
+
+st.title("Data Loader")
+
+uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+
+    st.write("Original data:")
+    st.dataframe(df)
+
+
+    df['Total Compensation'] = df['Salary'] + df['Bonus']
+
+    st.write("With Total Compensation column:")
+    st.dataframe(df)
+
+    csv = df.to_csv(index=False).encode('utf-8')
+
+
+    st.download_button(
+        label="Download processed CSV",
+        data=csv,
+        file_name='processed_data.csv',
+        mime='text/csv',
+    )
